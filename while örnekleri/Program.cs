4355ae421ae3443e3df94763610faee45c6ca37c
cansugunduzalp <cansugunduzalp@gmail.com>
2024-12-31 00:37:08 +0300

    Add project files.

---
+++ while örnekleri/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 0;
+
+        // while döngüsü ile 10 kere yazdırma
+        while (i < 10)
+        {
+            Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+            i++; // i'yi bir artırarak döngü sayısını kontrol ederiz
+        }
+    }
+}
